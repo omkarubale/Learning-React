18ed2980630549137cdfb369d6b1217aada268eb
omkarubale <oubale@gmail.com>
2019-03-22 14:21:37 +0530

    LEARNING: Components - Handling events

--- Counter Application/counter-app/src/components/counter.jsx
+++ Counter Application/counter-app/src/components/counter.jsx
@@ -2,21 +2,30 @@ import React, { Component } from "react";
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
+        tags: ["tag1", "tag2", "tag3"]
     };
 
     render() {
         return (
             <React.Fragment>
-                <span>{this.formatCount()}</span>
-                <button>Increment</button>
+                <span className={this.getBadgeClasses()}>
+                    {this.formatCount()}
+                </span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>
         );
     }
 
+    getBadgeClasses() {
+        let classes = "badge m-2 ";
+        classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
+        return classes;
+    }
+
     formatCount() {
         const { count } = this.state;
-        return count === 0 ? <h1>Zero</h1> : count;
+        return count === 0 ? "Zero" : count;
     }
 }
 
