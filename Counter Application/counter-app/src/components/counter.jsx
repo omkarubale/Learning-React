eebb43d47be235353c03cf3b938f830e50602203
omkarubale <oubale@gmail.com>
2019-03-23 01:43:07 +0530

    LEARNING: Completed Components section

--- Counter Application/counter-app/src/components/counter.jsx
+++ Counter Application/counter-app/src/components/counter.jsx
@@ -6,13 +6,25 @@ class Counter extends Component {
         tags: ["tag1", "tag2", "tag3"]
     };
 
+    handleIncrement = e => {
+        console.log(e + 1);
+        this.setState({ count: this.state.count + 1 });
+    };
+
     render() {
         return (
             <React.Fragment>
                 <span className={this.getBadgeClasses()}>
                     {this.formatCount()}
                 </span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
+                <button
+                    onClick={() => {
+                        this.handleIncrement(this.state.count);
+                    }}
+                    className="btn btn-secondary btn-sm"
+                >
+                    Increment
+                </button>
             </React.Fragment>
         );
     }
